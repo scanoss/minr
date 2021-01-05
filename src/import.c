3484ab1a5595fccc1df38f9d6acb158168709d3f
scanoss <support@scanoss.com>
2021-01-05 15:56:02 +0100

    popularity importation

--- src/import.c
+++ src/import.c
@@ -614,4 +614,15 @@ void mined_import(char *mined_path, bool skip_sort, bool erase)
 			ldb_import_csv(file_path, "quality", 3, false, 2 * MD5_LEN + 4, 1024, erase);
 		}
 	}
+
+	/* Import popularity. 7 CSV fields expected (id, 3 date_stamps, 3 integers) */
+	sprintf(file_path, "%s/popularity.csv", mined_path);
+	if (is_file(file_path))
+	{
+		printf("Importing %s\n", file_path);
+		if (csv_sort(file_path, skip_sort))
+		{
+			ldb_import_csv(file_path, "popularity", 7, false, 2 * MD5_LEN + 7, 1024, erase);
+		}
+	}
 }
