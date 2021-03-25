72ad4de5835304316826d04d6dc2db9b4885fcb0
scanoss <support@scanoss.com>
2021-03-25 16:57:40 +0100

    adds url.c

---
+++ inc/url.h
@@ -0,0 +1,5 @@
+#ifndef __URL_H
+    #define __URL_H
+
+void url_download(struct minr_job *job);
+#endif
---
+++ src/url.c
@@ -0,0 +1,168 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * src/url.c
+ *
+ * URL handling routines
+ *
+ * Copyright (C) 2018-2021 SCANOSS.COM
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <ctype.h>
+#include <openssl/md5.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "file.h"
+#include "minr.h"
+#include "ldb.h"
+
+/* Calculate md5 of "component/vendor/version" */
+void version_md5(struct minr_job *job)
+{
+	/* Clear memory */
+	memset(job->version_md5, 0, 16);
+
+	/* Extract vendor and component */
+	char vendor[MAX_ARG_LEN] = "\0";
+	char component[MAX_ARG_LEN] = "\0";
+	char version[MAX_ARG_LEN] = "\0";
+
+	/* Extract vendor and component from metadata */
+	extract_csv(vendor, job->metadata, 1, MAX_ARG_LEN);
+	extract_csv(component, job->metadata, 2, MAX_ARG_LEN);
+	extract_csv(version, job->metadata, 3, MAX_ARG_LEN);
+
+	if (!*vendor || !*component || !*version) return;
+	if (strlen(component) + strlen(vendor) + strlen(version) + 3 >= 1024) return;
+
+	/* Calculate md5 */
+	char md5[MAX_PATH_LEN] = "\0";
+
+	/* Calculate pair_md5 */
+	sprintf(md5, "%s/%s/%s", vendor, component, version);
+	for (int i = 0; i < strlen(md5); i++) md5[i] = tolower(md5[i]);
+	MD5((uint8_t *)md5, strlen(md5), job->version_md5);
+	ldb_bin_to_hex(job->version_md5, MD5_LEN, job->versionid);
+}
+
+void url_add(struct minr_job *job)
+{
+	char path[MAX_PATH_LEN]="\0";
+	sprintf(path, "%s/urls.csv", job->mined_path);
+
+	FILE *fp = fopen(path, "a");
+	if (!fp)
+	{
+		printf("Cannot create file %s\n", path);
+		exit(EXIT_FAILURE);
+	}
+	fprintf(fp, "%s,%s,%s\n", job->urlid, job->metadata, job->url);
+	fclose(fp);
+
+	/* Load license from metadata */
+	extract_csv(job->license, job->metadata, 5, MAX_ARG_LEN);
+	if (*job->license)
+	{
+		/* Load version_md5 and versionid */
+		version_md5(job);
+
+		sprintf(path, "%s/licenses.csv", job->mined_path);
+		FILE *fp = fopen(path, "a");
+		if (!fp)
+		{
+			printf("Cannot create file %s\n", path);
+			exit(EXIT_FAILURE);
+		}
+		fprintf(fp, "%s,0,%s\n", job->versionid, job->license);
+		fclose(fp);
+	}
+}
+
+void rm_tmpdir(struct minr_job *job)
+{
+	/* Assemble command */
+	char command[MAX_PATH_LEN] = "\0";
+	sprintf(command, "rm -rf %s", job->tmp_dir);
+
+	/* Execute command */
+	FILE *fp = popen(command, "r");
+	pclose(fp);
+}
+
+void url_download(struct minr_job *job)
+{
+	job->src = calloc(MAX_FILE_SIZE + 1, 1);
+	job->zsrc = calloc((MAX_FILE_SIZE + 1) * 2, 1);
+
+	/* Reserve memory for snippet fingerprinting */
+	if (!job->exclude_mz)
+	{
+		buffer = malloc(BUFFER_SIZE * 256);
+		hashes = malloc(MAX_FILE_SIZE);
+		lines  = malloc(MAX_FILE_SIZE);
+
+		/* Reserve memory for mz_cache for mined/sources (65536 files) */
+		job->mz_cache = malloc(MZ_FILES * sizeof(struct mz_cache_item));
+		for (int i = 0; i < MZ_FILES; i++) job->mz_cache[i].length = 0;
+	}
+
+	/* Open all file handlers in mined/files (256 files) */
+	out_file = open_file(job->mined_path);
+
+	/* Create temporary component directory */
+	sprintf(job->tmp_dir,"%s/minr-%d", tmp_path, getpid());
+	mkdir(job->tmp_dir, 0755);
+
+	/* urlid will contain the hex md5 of the entire component */
+	bool downloaded = download(job);
+
+	/* Process downloaded/expanded directory */
+	if (is_dir(job->tmp_dir) && downloaded)
+	{
+		/* Add info to urls.csv */
+		url_add(job);
+
+		recurse(job, job->tmp_dir);
+
+		rm_tmpdir(job);
+	}
+
+	else
+	{
+		printf("Capture failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	/* Close files */
+	for (int i=0; i < 256; i++) fclose(out_file[i]);
+
+	if (!job->exclude_mz)
+	{
+		/* Flush mz cache */
+		mz_flush(job->mined_path, job->mz_cache);
+
+		free(job->mz_cache);
+		free(buffer);
+		free(hashes);
+		free(lines);
+	}
+
+	free(out_file);
+	free(job->src);
+	free(job->zsrc);
+}
